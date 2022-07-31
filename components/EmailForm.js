d03ae11fa0e1b525321aff3bba870f469496385d
shadid12 <shadidhaque2012@gmail.com>
2022-07-31 19:35:02 -0400

    added mail email collector

---
+++ components/EmailForm.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+const btnStyle = "inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
+export default function EmailForm() {
+  const [email, setEmail] = useState('');
+  return (
+    <div>
+      <input 
+        type="text" 
+        placeholder="Email" 
+        className="mb-2 mt-1 border p-2 border-slate-400 w-[32rem] rounded-md"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <br />
+      <button 
+        type="button" 
+        className={btnStyle}
+        onClick={() => {
+          setEmail('');
+        }}
+      >
+        Subscribe
+      </button>
+    </div>
+  );
+}
