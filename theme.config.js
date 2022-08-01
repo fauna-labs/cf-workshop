aaaed58bf3995ee472824ef3105a58aec7f30880
shadid12 <shadidhaque2012@gmail.com>
2022-08-01 08:31:58 -0400

    added chapter one

--- theme.config.js
+++ theme.config.js
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Fauna-AWS Workshop" />
+      <meta name="og:description" content="Fauna-AWS Workshop" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
+      <meta name="twitter:site:domain" content="fauna.com" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="og:title" content="Fauna-AWS Workshop" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="Fauna-AWS Workshop" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
