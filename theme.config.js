78d25741db02ed710cc005d3bd42c933a61efc19
shadid12 <shadidhaque2012@gmail.com>
2022-08-12 08:44:13 -0400

    trying metatag deployment

--- theme.config.js
+++ theme.config.js
@@ -13,7 +13,7 @@ export default {
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
+      {/* <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
@@ -21,13 +21,41 @@ export default {
       <meta name="og:description" content="Fauna-AWS Workshop" />
       <meta name="twitter:card" content="https://workshop-shadid.s3.amazonaws.com/aws-workshop-cover.png" />
       <meta name="twitter:image:secure_url" content="https://workshop-shadid.s3.amazonaws.com/aws-workshop-cover.png" />
-      <meta name="twitter:title" content="Fauna AWS Workshop ~ Building an event driven app with AWS and Fauna" />
+      <meta name="twitter:title" content="Fauna AWS Workshop ~ Building an event driven app with AWS services and Fauna" />
       <meta name="twitter:description" content="Fauna-AWS Workshop" />
       <meta name="twitter:site:domain" content="fauna.com" />
       <meta name="twitter:url" content="https://fauna.com" />
       <meta name="og:title" content="Fauna-AWS Workshop" />
       <meta name="og:image" content="https://workshop-shadid.s3.amazonaws.com/aws-workshop-cover.png" />
-      <meta name="apple-mobile-web-app-title" content="Fauna-AWS Workshop" />
+      <meta name="apple-mobile-web-app-title" content="Fauna-AWS Workshop" /> */}
+
+
+      <title>Fauna AWS Workshop ~ Building an event-driven app with AWS services and Fauna</title>
+      <meta name="title" content="Fauna AWS Workshop ~ Building an event-driven app with AWS services and Fauna"/>
+      <meta name="description" content="This hands-on guide walks you through building a real-world event-driven serverless application using AWS services (i.e., AWS Lambda, Step Functions, API Gateway) and Fauna.
+
+      In this workshop, you build a vacation booking application (Similar to Kayak). "/>
+
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://metatags.io/"/>
+      <meta property="og:title" content="Fauna AWS Workshop ~ Building an event-driven app with AWS services and Fauna"/>
+      <meta property="og:description" content="This hands-on guide walks you through building a real-world event-driven serverless application using AWS services (i.e., AWS Lambda, Step Functions, API Gateway) and Fauna.
+
+      In this workshop, you build a vacation booking application (Similar to Kayak). "/>
+      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+
+      
+      <meta property="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:url" content="https://metatags.io/"/>
+      <meta property="twitter:title" content="Fauna AWS Workshop ~ Building an event-driven app with AWS services and Fauna"/>
+      <meta property="twitter:description" content="This hands-on guide walks you through building a real-world event-driven serverless application using AWS services (i.e., AWS Lambda, Step Functions, API Gateway) and Fauna.
+
+      In this workshop, you build a vacation booking application (Similar to Kayak). "/>
+      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+
+
+
+
       <link
         rel="apple-touch-icon"
         sizes="180x180"
