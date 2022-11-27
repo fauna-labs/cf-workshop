9a46a614d3427a4f6854d291b98475c80cecbe98
shadid12 <shadidhaque2012@gmail.com>
2022-11-26 22:11:39 -0500

    done with chapter one

--- theme.config.js
+++ theme.config.js
@@ -7,7 +7,7 @@ export default {
     <>
       <img style={{ width: '100px' }} className="mr-2 font-extrabold hidden md:inline" src="https://images.ctfassets.net/po4qc9xpmpuh/7itYmeRxmVGIXwwGWHrQU3/42f3e7fa7d39fab5b6222f6199f0203c/Fauna_Logo.svg" alt="Nextra" />
       <span className="text-gray-600 font-normal hidden md:inline" style={{ marginTop: '10px' }}>
-        AWS Fauna Workshop
+        Cloudflare Fauna Workshop
       </span>
     </>
   ),
@@ -15,27 +15,21 @@ export default {
     <>
 
 
-      <title>Fauna AWS Workshop ~ Building an event-driven app with AWS services and Fauna</title>
+      <title>Fauna Cloudflare Workshop ~ Build a globally distributed full-stack app using Cloudflare Pages, Next.js and Fauna</title>
       <meta name="title" content="Fauna AWS Workshop ~ Building an event-driven app with AWS services and Fauna"/>
-      <meta name="description" content="This hands-on guide walks you through building a real-world event-driven serverless application using AWS services (i.e., AWS Lambda, Step Functions, API Gateway) and Fauna.
-
-      In this workshop, you build a vacation booking application (Similar to Kayak). "/>
+      <meta name="description" content="This hands-on guide walks you through building a globally distributed full-stack application with Cloudflare Pages + Fauna"/>
 
       <meta property="og:type" content="website"/>
       <meta property="og:url" content="https://fauna.com"/>
       <meta property="og:title" content="Fauna AWS Workshop ~ Building an event-driven app with AWS services and Fauna"/>
-      <meta property="og:description" content="This hands-on guide walks you through building a real-world event-driven serverless application using AWS services (i.e., AWS Lambda, Step Functions, API Gateway) and Fauna.
-
-      In this workshop, you build a vacation booking application (Similar to Kayak). "/>
+      <meta property="og:description" content="This hands-on guide walks you through building a globally distributed full-stack application with Cloudflare Pages + Fauna"/>
       <meta property="og:image" content="https://workshop-shadid.s3.amazonaws.com/aws-workshop-cover.png"/>
 
       <meta name="twitter:site" content="@fauna" />
       <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:url" content="https://fauna.com"/>
       <meta property="twitter:title" content="Fauna AWS Workshop ~ Building an event-driven app with AWS services and Fauna"/>
-      <meta property="twitter:description" content="This hands-on guide walks you through building a real-world event-driven serverless application using AWS services (i.e., AWS Lambda, Step Functions, API Gateway) and Fauna.
-
-      In this workshop, you build a vacation booking application (Similar to Kayak). "/>
+      <meta property="twitter:description" content="This hands-on guide walks you through building a globally distributed full-stack application with Cloudflare Pages + Fauna"/>
       <meta property="twitter:image" content="https://workshop-shadid.s3.amazonaws.com/aws-workshop-cover.png"/>
 
 
